27182a9a5ccdbccc339232e64e97581c9ae2ab45
Nadipellianvith <nadipelly2902@gmail.com>
2024-01-11 11:15:48 +0530

    first

---
+++ src/main/java/Mavenjavaproject/javaa/App.java
@@ -0,0 +1,13 @@
+package Mavenjavaproject.javaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
